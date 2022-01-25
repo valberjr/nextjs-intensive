815d675371fdfb834c69a3b6e386b45e37cfcec5
valberjr <3cifhzht@duck.com>
2022-01-25 15:26:43 -0300

    modularized  css

---
+++ fundamentals/src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/EStiloso.module.css";
+
+export default function Estiloso() {
+  return (
+    <div className={styles.purple}>
+      <h1>Style using CSS Module</h1>
+    </div>
+  );
+}
