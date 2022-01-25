572e7561010ad0bcfd5d192a7992bf1954ec60bb
valberjr <3cifhzht@duck.com>
2022-01-25 16:59:26 -0300

    simple navigation

--- fundamentals/src/pages/index.jsx
+++ fundamentals/src/pages/index.jsx
@@ -14,6 +14,7 @@ export default function Home() {
       <Navigator text='Stylish' destiny='/stylish' />
       <Navigator text='Example' destiny='/example' color='#9400d3' />
       <Navigator text='JSX' destiny='/jsx' color='crimson' />
+      <Navigator text='Navigation #01' destiny='/navigation' color='green' />
     </div>
   );
 }
---
+++ fundamentals/src/pages/navigation/index.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout';
+
+export default function Index() {
+  return (
+    <Layout title='Navigation #01'>
+      <div>
+        <h1>Naviagation #01</h1>
+      </div>
+    </Layout>
+  );
+}
