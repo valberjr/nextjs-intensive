48646c4c8091e53c291571398af3cfea02545380
valberjr <3cifhzht@duck.com>
2022-01-25 15:37:02 -0300

    using components

---
+++ fundamentals/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Nextjs and React Fundamentals</h1>
+        </header>       
+    )
+}
---
+++ fundamentals/src/pages/example.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function Example() {
+  return (
+    <>
+      <Header />
+      <Header />
+    </>
+  );
+}
