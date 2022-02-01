e27cf02962f352fcb2441da2bc029d9f9c6990c8
v4lb3r <3cifhzht@duck.com>
2022-02-01 10:17:10 -0300

    button component

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,37 +1,43 @@
+import Button from '../components/Button';
 import Layout from '../components/Layout';
 import Table from '../components/Table';
 import Client from '../core/Client';
 
 export default function Home() {
-  const clients = [
-    new Client('Ana', 34, '1'),
-    new Client('Bia', 45, '2'),
-    new Client('Carlos', 23, '3'),
-    new Client('Pedro', 44, '4'),
-  ];
+    const clients = [
+        new Client('Ana', 34, '1'),
+        new Client('Bia', 45, '2'),
+        new Client('Carlos', 23, '3'),
+        new Client('Pedro', 44, '4'),
+    ];
 
-  function selectedClient(client: Client) {
-    console.log(client.name);
-  }
-  function deletedClient(client: Client) {
-    console.log(`deleting ... ${client.name}`);
-  }
+    function selectedClient(client: Client) {
+        console.log(client.name);
+    }
+    function deletedClient(client: Client) {
+        console.log(`deleting ... ${client.name}`);
+    }
 
-  return (
-    <div
-      className={`
-      flex justify-center items-center h-screen
-      bg-gradient-to-r from-blue-500 to-purple-500
-      text-white
-    `}
-    >
-      <Layout title="Simple Registration">
-        <Table
-          clients={clients}
-          selectedClient={selectedClient}
-          deletedClient={deletedClient}
-        />
-      </Layout>
-    </div>
-  );
+    return (
+        <div
+            className={`
+              flex justify-center items-center h-screen
+              bg-gradient-to-r from-blue-500 to-purple-500
+              text-white
+        `}
+        >
+            <Layout title="Simple Registration">
+                <div className="flex justify-end">
+                    <Button color="green" className="mb-4">
+                        New Client
+                    </Button>
+                </div>
+                <Table
+                    clients={clients}
+                    selectedClient={selectedClient}
+                    deletedClient={deletedClient}
+                />
+            </Layout>
+        </div>
+    );
 }
