7e3a4f79213d216801bfd7a08f9ece7f3a853212
v4lb3r <3cifhzht@duck.com>
2022-02-02 09:00:21 -0300

    client repository

--- next-crud/src/firebase/config.ts
+++ next-crud/src/firebase/config.ts
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
-if (!firebase.getApps.length) {
+if (!firebase.apps.length) {
     firebase.initializeApp({
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
